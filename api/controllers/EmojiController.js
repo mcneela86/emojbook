98f893a8deb0bd24c435f5602427cf9adb0e2980
Martin McNeela <mcneela86@gmail.com>
2015-10-24 20:23:41 +1100

    Add mongo support

--- api/controllers/EmojiController.js
+++ api/controllers/EmojiController.js
@@ -7,5 +7,13 @@
 
 module.exports = {
 	
+	// These are included by default (provided the is a coresponding model)
+	// they are refered to as 'blueprint action' 
+	// find: function (req, res) {},
+	// findOne: function (req, res) {},
+	// create: function (req, res) {},
+	// update: function (req, res) {},
+	// destroy: function (req, res) {},
+
 };
 
