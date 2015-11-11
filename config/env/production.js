19d8b9fce6d3e2dd68561e0f6a2a31fa5151146c
Martin McNeela <mcneela86@gmail.com>
2015-11-11 14:28:24 +1100

    Configure for mongolab.

--- config/env/production.js
+++ config/env/production.js
@@ -19,7 +19,7 @@ module.exports = {
 
   // models: {
   //   connection: 'someMysqlServer'
-  
+    connection: 'remoteMongolabMongoDB'
   // },
 
   /***************************************************************************
