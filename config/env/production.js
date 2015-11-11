d8d27d8fd1ba687da08a13273522206096fb96e4
Martin McNeela <mcneela86@gmail.com>
2015-11-11 14:04:40 +1100

    Configure for mongolab.

--- config/env/production.js
+++ config/env/production.js
@@ -17,9 +17,10 @@ module.exports = {
    * environment (see config/connections.js and config/models.js )           *
    ***************************************************************************/
 
-  // models: {
+  models: {
   //   connection: 'someMysqlServer'
-  // },
+    connection: 'remoteMongolabMongoDB'
+  },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
